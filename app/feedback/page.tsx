5befd816b8e00e86a367973099f0bd523a85aca3
lester-td <lester.t.d@outlook.com>
2026-07-10 13:24:49 +0800

    Render DB-backed home and feedback pages dynamically

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -15,6 +15,8 @@ export const metadata: Metadata = {
   },
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function FeedbackPage()
 {
   const [semesterTree, latestDataUpdatedAt] = await Promise.all([
--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,4 @@
+export const dynamic = "force-dynamic";
+
 export { metadata } from "./home";
 export { default } from "./home";
